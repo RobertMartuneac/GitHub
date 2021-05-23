21dfadc6698aec322edfd2f33dbc5eddf79b6f50
Robert Ionuț Marțuneac <robert.martuneac@student.usv.ro>
2021-05-23 23:59:41 +0300

    Proiect Piu Lab 6

---
+++ Data/AdministrareProduse.cs
@@ -0,0 +1,195 @@
+﻿using System;
+using System.Collections;
+using Patiserie.Librarie;
+using System.IO;
+using System.Collections.Generic;
+
+
+namespace Data
+{
+    public class AdministrareProduse : IStocareData
+    {
+        string NumeFisier { get; set; }
+
+        public AdministrareProduse()
+        {
+            this.NumeFisier = "Produse.txt";
+            Stream FisierText = File.Open(NumeFisier, FileMode.OpenOrCreate);
+            FisierText.Close();
+
+            //creaza sau deschide fisierul text
+        }
+        public void AddProdus(Produse p)
+        {
+            this.NumeFisier = "Produse.txt";
+            try
+            {
+                //scriere in fisier a unui produs
+                using(StreamWriter scriereFisier=new StreamWriter(NumeFisier, true))
+                {
+                    scriereFisier.WriteLine(p.ConversieLaSir_Fisier());
+                }
+            }
+            catch (IOException eIO)
+            {
+                throw new Exception("Eroare la deschiderea fisierului. Mesaj: " + eIO.Message);
+            }
+            catch (Exception eGen)
+            {
+                throw new Exception("Eroare generica. Mesaj: " + eGen.Message);
+            }
+        }
+        public ArrayList GetInfoProduse()
+        {
+            ArrayList a_produse = new ArrayList();
+            this.NumeFisier = "Produse.txt";
+
+            try
+            {
+                // instructiunea 'using' va apela sr.Close()
+                using (StreamReader f = new StreamReader(NumeFisier))
+                {
+                    string line;
+                    //citeste cate o linie si creaza un obiect de tip Student pe baza datelor din linia citita
+                    while ((line = f.ReadLine()) != null)
+                    {
+                        Produse ProduseDinFisier = new Produse(line);
+                        a_produse.Add(ProduseDinFisier);
+                    }
+                }
+            }
+            catch (IOException eIO)
+            {
+                throw new Exception("Eroare la deschiderea fisierului. Mesaj: " + eIO.Message);
+            }
+            catch (Exception eGen)
+            {
+                throw new Exception("Eroare generica. Mesaj: " + eGen.Message);
+            }
+
+            return a_produse;
+        }
+        public List<Produse> GetInfoProduseList()
+        {
+            List<Produse> a_produse = new List<Produse>();
+            this.NumeFisier = "Produse.txt";
+
+            try
+            {
+                // instructiunea 'using' va apela sr.Close()
+                using (StreamReader f = new StreamReader(NumeFisier))
+                {
+                    string line;
+                    //citeste cate o linie si creaza un obiect de tip Student pe baza datelor din linia citita
+                    while ((line = f.ReadLine()) != null)
+                    {
+                        Produse ProduseDinFisier = new Produse(line);
+                        a_produse.Add(ProduseDinFisier);
+                    }
+                }
+            }
+            catch (IOException eIO)
+            {
+                throw new Exception("Eroare la deschiderea fisierului. Mesaj: " + eIO.Message);
+            }
+            catch (Exception eGen)
+            {
+                throw new Exception("Eroare generica. Mesaj: " + eGen.Message);
+            }
+
+            return a_produse;
+        }
+        public Produse GetProdus(string nume)
+        {
+            try
+            {
+                // instructiunea 'using' va apela sr.Close()
+                using (StreamReader f = new StreamReader(NumeFisier))
+                {
+                    string line;
+
+                    //citeste cate o linie si creaza un obiect de tip Student pe baza datelor din linia citita
+                    while ((line = f.ReadLine()) != null)
+                    {
+                        Produse PrFisier = new Produse(line);
+                        if (PrFisier.Nume.ToLower() == nume)
+                            return PrFisier;
+                    }
+                }
+            }
+            catch (IOException eIO)
+            {
+                throw new Exception("Eroare la deschiderea fisierului. Mesaj: " + eIO.Message);
+            }
+            catch (Exception eGen)
+            {
+                throw new Exception("Eroare generica. Mesaj: " + eGen.Message);
+            }
+
+            return null;
+        }
+        public Produse GetProdusId(int id)
+        {
+            try
+            {
+                // instructiunea 'using' va apela sr.Close()
+                using (StreamReader f = new StreamReader(NumeFisier))
+                {
+                    string line;
+
+                    //citeste cate o linie si creaza un obiect de tip Student pe baza datelor din linia citita
+                    while ((line = f.ReadLine()) != null)
+                    {
+                        Produse PrFisier = new Produse(line);
+                        if (PrFisier.NrProdus == id)
+                            return PrFisier;
+                    }
+                }
+            }
+            catch (IOException eIO)
+            {
+                throw new Exception("Eroare la deschiderea fisierului. Mesaj: " + eIO.Message);
+            }
+            catch (Exception eGen)
+            {
+                throw new Exception("Eroare generica. Mesaj: " + eGen.Message);
+            }
+
+            return null;
+        }
+        public bool UpdateProdus(string _id, string _pret)
+        {
+            this.NumeFisier = "Produse.txt";
+            bool verificare = false;
+            ArrayList prod = new ArrayList();
+
+            using (StreamReader s = new StreamReader(NumeFisier))
+            {
+                string line;
+
+                //citeste cate o linie si creaza un obiect de tip Student pe baza datelor din linia citita
+                while ((line = s.ReadLine()) != null)
+                {
+                    Produse produsDinFisier = new Produse(line);
+                    if (produsDinFisier.NrProdus == Convert.ToInt32(_id))
+                    { produsDinFisier.Pret = _pret; verificare = true; }
+
+                    prod.Add(produsDinFisier);
+                }
+            }
+            if (verificare == true)
+            {
+                File.Delete(NumeFisier);
+                IStocareData stocareprodus = new AdministrareProduse();
+                for (int i = 0; i < prod.Count; i++)
+                {
+                    stocareprodus.AddProdus((Produse)prod[i]);
+                }
+                return true;
+            }
+            else
+                return false;
+        }
+
+    }
+}
---
+++ Patiserie.Librarie/Produse.cs
@@ -0,0 +1,142 @@
+﻿using System;
+
+
+namespace Patiserie.Librarie
+{
+    public class Produse
+    {
+        private const char SEPARATOR_PRINCIPAL_FISIER = ';';
+
+        public static int NrUltimProdus { get; set; } = 0;
+        public int NrProdus { get; set; }
+        public string Nume { get; set; }
+        public string Pret { get; set; }
+        public string Ingrediente { get; set; }
+        public string Valabilitate { get; set; }
+        public int Cantitate { get; set; }
+        public TipProdus TipP { get; set; }
+
+        //constructor implicit
+        public Produse()
+        {
+            Nume = Pret = Ingrediente = Valabilitate = string.Empty;
+            Cantitate = 0;
+            NrUltimProdus++;
+            NrProdus=NrUltimProdus;
+        }
+        //constructor
+        //public Produse(string text)
+        //{
+        //    string[] produse = text.Split(',');
+
+        //    if (produse.Length != 5)
+        //    {
+        //        Nume = Pret = Ingrediente = Valabilitate = string.Empty;
+        //        Cantitate = 0;
+        //        Console.WriteLine("NU S-AU INTRODUS TOATE DATELE NECESARE PENTRU ACEST PRODUS");
+        //    }
+        //    else
+        //    {
+        //        Nume = produse[0];
+        //        Pret = produse[1];
+        //        Ingrediente = produse[2];
+        //        Valabilitate = produse[3];
+        //        Cantitate = Convert.ToInt32(produse[4]);
+        //        NrUltimProdus++;
+        //        NrProdus = NrUltimProdus;
+        //    }
+        //}
+        public Produse(string nume, string pret, string ingrediante, string valabilitate, int cantitate)
+        {
+            Nume = nume;
+            Pret = pret;
+            Ingrediente = ingrediante;
+            Valabilitate = valabilitate;
+            Cantitate = cantitate;
+            NrUltimProdus++;
+            NrProdus = NrUltimProdus;
+        }
+
+        //constructor linie fisier
+        public Produse(string linie)
+        {
+            string[] file_data = linie.Split(SEPARATOR_PRINCIPAL_FISIER);
+
+            NrProdus = Convert.ToInt32(file_data[(int)CampuriProdus.ID]);
+            Nume = file_data[(int)CampuriProdus.Nume];
+            Pret = file_data[(int)CampuriProdus.Pret];
+            Ingrediente = file_data[(int)CampuriProdus.Ingrediente];
+            Valabilitate = file_data[(int)CampuriProdus.Valabilitate];
+            Cantitate = Convert.ToInt32(file_data[(int)CampuriProdus.Cantitate]);
+            TipP = (TipProdus)Convert.ToInt32(file_data[(int)CampuriProdus.Tip]);
+            NrUltimProdus = NrProdus;
+            //NrUltimProdus++;
+            //NrProdus = NrUltimProdus;
+
+        }
+        public string ConversieLaSir()
+        {
+            string aux;
+            if (string.IsNullOrEmpty(Nume))
+            {
+                aux = "Warning! Nu sunt produse de afisat!";
+            }
+            else
+            {
+                aux = $"Nume produs: {Nume}\n" +
+                $"Pret produs: {Pret}\n" +
+                $"Ingrediente: {Ingrediente}\n" +
+                $"Valabilitate: {Valabilitate}\n" +
+                $"Cantitate: {Cantitate} bucati\n";
+            }
+
+            return aux;
+        }
+        public string ConversieLaSir_Afisare()
+        {
+            string aux;
+
+            if ((int)TipP == 2)
+            {
+                aux = $"[{NrProdus}] Nume: {Nume} -> Pret: {Pret}\n" +
+                $"Valabilitate: {Valabilitate}\n" +
+                $"Cantitate: {Cantitate} bucati\n" +
+                $"Tip produs: {TipP}\n";
+            }
+            else
+            {
+                aux = $"[{NrProdus}] Nume: {Nume} -> Pret: {Pret}\n" +
+                $"Ingrediente: {Ingrediente};\n" +
+                $"Valabilitate: {Valabilitate}\n" +
+                $"Cantitate: {Cantitate} bucati\n" +
+                $"Tip produs: {TipP}\n";
+            }
+
+            return aux;
+        }
+        public string ConversieLaSir_Fisier()
+        {
+            string aux;
+
+            aux = string.Format("{1}{0}{2}{0}{3}{0}{4}{0}{5}{0}{6}{0}{7}",
+                SEPARATOR_PRINCIPAL_FISIER, NrProdus.ToString(), (Nume ?? " Necunoscut "), (Pret ?? " Necunoscut "), (Ingrediente ?? " Necunoscut "), (Valabilitate ?? " Necunoscut "), Cantitate.ToString(), (int)TipP);
+            return aux;
+        }
+
+        //comparare a doua produse pretul
+        public void Compare(Produse pr1)
+        {
+            if (this.Cantitate > pr1.Cantitate)
+            {
+                Console.WriteLine($"Sunt mai multe produse de tipul {this.Nume} decat {pr1.Nume}");
+            }
+            else
+            {
+                if (this.Cantitate < pr1.Cantitate)
+                    Console.WriteLine($"Sunt mai multe produse de tipul {pr1.Nume} decat {this.Nume}");
+                else
+                    Console.WriteLine("Produsele sunt in cantitati egale");
+            }
+        }
+    }
+}
